84887340e48aea6f5f2aaf3276d3b9b806edca8d
sapiensmagno <sapiensmagno@bol.com.br>
2015-09-22 22:42:00 -0300

    send_keys method created. It sends commands or regular strings.

--- EHLLAPI.py
+++ EHLLAPI.py
@@ -1,6 +1,7 @@
 from ctypes import c_int, c_char_p, c_void_p, byref, WinDLL, WINFUNCTYPE
 
 class Emulator(object):
+       
     hllApi = None
     
     def _func_num(self, arg):
@@ -37,6 +38,25 @@ class Emulator(object):
     def disconnect_presentation_space(self):
         return self.hllApi (byref(self._func_num(2)), self._data_str("A"), byref(self._lenght(1)), byref(self._ps_position(0)))
         
-    def sendpf23(self):
-        return self.hllApi (byref(self._func_num(3)), self._data_str("@n"), byref(self._lenght(2)), byref(self._ps_position(0)))
+    def send_keys(self, keys):
+        # Cases:  the argument ''keys'' is just a regular string, it is a string inside <> or it is inside <> because it is a special command.
+        # TODO: handle return codes
+        # TODO: if it's not a command, use Copy String to Field (33) or Copy String to Presentation Space (15) to send string
+        # TODO: split strings when len(keys) > 255     
+        snd_txt = keys 
+        txt_lenght = len(snd_txt)
+        keyboard_mnemonics = {'TAB':'@T', 'ENTER':'@E', 'ERASEEOF':'@F', 'BACKSPACE':'@<', 'BACKTAB':'@B', 'CLEAR':'@C', 'DOWN':'@V', 'LEFT':'@L', 'RIGHT':'@Z', 'CURSEL':'@A@J', 'UP':'@U', 'DELETE':'@D', 'PF1':'@1', 'PF2':'@2', 'PF3':'@3', 'PF4':'@4', 'PF5':'@5', 'PF6':'@6', 'PF7':'@7', 'PF8':'@8', 'PF9':'@9', 'PF10':'@a', 'PF11':'@b', 'PF12':'@c', 'PF13':'@d', 'PF14':'@e', 'PF15':'@f', 'PF16':'@g', 'PF17':'@h', 'PF18':'@i', 'PF19':'@j', 'PF20':'@k', 'PF21':'@l', 'PF22':'@m', 'PF23':'@n', 'PF24':'@o', 'PA1':'@x', 'PA2':'@y', 'PA3':'@z', 'PAGEUP':'@u', 'PAGEDN':'@v', 'RESET':'@R', 'SYSREQ':'@A@H', 'HELP':'@H', 'HOME':'@0', 'INSERT':'@I', 'NEWLINE':'@N', 'DUP':'@S@x', 'ERINP':'@A@F', 'FLDEXT':'@A@E', 'FIELDMARK':'@S@y', 'FIELD-':'@A@-', 'FIELD+':'@A@+'}
+        if snd_txt[0] == '<' and snd_txt[txt_lenght-1] == '>':
+            try:
+                snd_txt = keyboard_mnemonics[keys.strip('<>')] # Try to associate the string inside the <> with one object in the dictionary
+                txt_lenght = len(snd_txt)
+            except KeyError:
+                snd_txt = keys # Couldn't find a match in the dictionary.   
+        return self.hllApi (byref(self._func_num(3)), self._data_str(snd_txt), byref(self._lenght(txt_lenght)), byref(self._ps_position(0)))
+        
+    # TODO: 
+    # def getconnectionstatus
+    # def cursorcoordconversion
+    # def getcursorposition
+    # def setcursorposition
         
